672f5a86171242b5bce2631d3c28301390ac8417
prog-apprentice401 <koustubhsrivastava1@gmail.com>
2021-05-05 08:49:01 +0530

    Events now sorted
    The events added to the Events.log file are now sorted according to date

--- source/events.c
+++ source/events.c
@@ -9,7 +9,7 @@
 
 //function: gets event message from user
 //accepts : character array to store message
-//returns : 0 if all ok, 1 if message was too long
+//returns : 0:success;1:full message may not have been read
 int getEventMessage (char message[])
 {
 	int maxMessageLength = MAX_MESSAGE_LENGTH;
@@ -21,23 +21,6 @@ int getEventMessage (char message[])
 	if (charsRead >= maxMessageLength) {
 		return 1; //all cahracters may not have been read
 	}
-	return 0;
-}
-
-//function: adds Event to Events.log file
-//accepts : Event to add
-//returns : status of write
-int addEvent (Event event)
-{
-	FILE * eventsFile = fopen (EVENTS_FILE_PATH, "a+b");
-
-	if (eventsFile == NULL) {
-		setColour (RED);
-		printf ("error: addEvent(): could not open Events.log file");
-		return -1;
-	}
-	fwrite (&event, sizeof (Event), 1, eventsFile);
-	fclose (eventsFile);
 	return 0;
 }
 
@@ -67,6 +50,37 @@ int printEvents (Date date)
 	return eventSearchStatus;
 }
 
+//function: adds Event to Events.log file
+//accepts : Event to add
+//returns : 0:success;1:faliure;-1:file error
+int addEvent (Event eventToAdd)
+{
+	FILE* eventsFile = fopen (EVENTS_FILE_PATH, "r+b");
+	FILE* tempEventsFile = fopen (TEMP_EVENTS_FILE_PATH, "wb");
+	Event eventRead;
+	int eventAddStatus = 1; //faliure adding event
+
+	if (eventsFile == NULL || tempEventsFile == NULL) {
+		return eventAddStatus = -1;  //file error
+	}
+	while (fread (&eventRead, sizeof (Event), 1, eventsFile) == 1) {
+		fwrite (&eventRead, sizeof (Event), 1, tempEventsFile);
+
+		//adds event in ascending order
+		if (compareDates (eventRead.date, eventToAdd.date) >= 0) {
+			fwrite (&eventToAdd, sizeof (Event), 1, tempEventsFile);
+			eventAddStatus = 0;  //success
+		}
+	}
+	fclose (eventsFile);
+	fclose (tempEventsFile);
+
+	remove (EVENTS_FILE_PATH);
+	rename (TEMP_EVENTS_FILE_PATH, EVENTS_FILE_PATH);
+
+	return eventAddStatus;
+}
+
 //function: deletes ALL events on a particular day
 //accepts : Date object to delete
 //returns : 0:events found;1:no events found;-1:file error
@@ -78,9 +92,10 @@ int deleteEvents (Date date)
 	Event eventRead;
 
 	if (eventsFile == NULL || tempEventsFile == NULL) {
-		return deletionStatus = -1;	//file opening error
+		return deletionStatus = -1;	//file error
 	}
-	while (fread (&eventRead, sizeof (Event), 1, eventsFile) == 1) {
+	while (fread (&eventRead, sizeof (Event), 1,
+		eventsFile) == 1) {
 		if (compareDates (date, eventRead.date) != 0) {
 			fwrite (&eventRead, sizeof (Event), 1, tempEventsFile);
 		} else {  //this record is not in new file
